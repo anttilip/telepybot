070864d4a89111e835c8801c2248b9cf95a122e4
Antti Lipsanen <antti.lip@gmail.com>
2016-10-06 18:50:03 +0300

    Add stats module

---
+++ modules/stats.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from math import ceil
+from subprocess import check_output
+
+
+def handle_update(bot, update, **kwargs):
+    sent_time = update.message.date
+    now = datetime.now()
+    delta = now - sent_time
+
+    #report = 'Ping: {}ms\n'.format(ceil(delta.total_seconds() * 1000))
+    ping = ceil(delta.total_seconds() * 1000)
+    uptime = check_output(['uptime', '-p']).decode().split(' ', 1)[1]
+
+    output = check_output(['cat', '/proc/loadavg']).split()[1].decode()
+    load_5m = float(output) * 100
+
+    report = ("Ping: {}ms\nBot uptime: {}"
+              "Load over last 5 minutes: {}%").format(ping, uptime, load_5m)
+    #report += "Bot uptime: {}\nLoad over last 5 minutes: {}%".format(uptime,
+    #                                                                 load_5m)
+
+    bot.sendMessage(update.message.chat_id, text=report)
